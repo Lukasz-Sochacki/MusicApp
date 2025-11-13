4971a74b2c6613886f87b82ec98ea878a0f2ffea
Łukasz Sochacki <sochacki.luki@gmail.com>
2025-11-13 12:16:56 +0100

    Add subscribe section and subpages functions

---
+++ dist/js/settings.js
@@ -0,0 +1,18 @@
+export const select = {
+  templateOf: {},
+  containerOf: {
+    pages: '#pages',
+  },
+  nav: {
+    links: '.navigation__menu li a',
+  },
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
